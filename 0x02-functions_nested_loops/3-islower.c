dadb1f2df509269703e31b7546a477a04026ed17
akubosamuel <akubosamuel3615@gmail.com>
2023-02-22 12:34:41 -0800

    g

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -4,31 +4,20 @@
 <<<<<<< HEAD
  * _islower - function to print lowercase
  *
- *@c: parameter to be printed
+ * c: parameter to be printed
  *
  * Return: 1 if it is a lowercase
  * and 0 otherwise
  */
 
-
-=======
- *  _islower - function to print ower case
- *
- *  0c: parameter to be printed
- *
- *  Return: 1 if lowercase
- *  and 0 otherwise
- */
-
->>>>>>> 30ad21922e49fd714a8282c139de0a1521dfacdc
 int _islower(int c)
 {
 	if (c >= 97 && c <= 122)
+	{
 		return (1);
+	}
 	else
+	{
 		return (0);
+	}
 }
-<<<<<<< HEAD
-=======
-
->>>>>>> 30ad21922e49fd714a8282c139de0a1521dfacdc
